ef45f9e93ea716e8626c586d8933e05220e9b82d
Behram CENGİZOĞLU <64408522+BehramCENGIZOGLU@users.noreply.github.com>
2021-05-01 19:07:45 +0300

    Paint Programi

---
+++ PaintProgram/PaintProgram/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace PaintProgram
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dosyaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.yeniToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.yükleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kaydetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.çıkışToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dosyaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(669, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // dosyaToolStripMenuItem
+            // 
+            this.dosyaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.yeniToolStripMenuItem,
+            this.yükleToolStripMenuItem,
+            this.kaydetToolStripMenuItem,
+            this.çıkışToolStripMenuItem});
+            this.dosyaToolStripMenuItem.Name = "dosyaToolStripMenuItem";
+            this.dosyaToolStripMenuItem.Size = new System.Drawing.Size(64, 24);
+            this.dosyaToolStripMenuItem.Text = "Dosya";
+            // 
+            // yeniToolStripMenuItem
+            // 
+            this.yeniToolStripMenuItem.Name = "yeniToolStripMenuItem";
+            this.yeniToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.yeniToolStripMenuItem.Text = "Yeni";
+            // 
+            // yükleToolStripMenuItem
+            // 
+            this.yükleToolStripMenuItem.Name = "yükleToolStripMenuItem";
+            this.yükleToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.yükleToolStripMenuItem.Text = "Yükle";
+            // 
+            // kaydetToolStripMenuItem
+            // 
+            this.kaydetToolStripMenuItem.Name = "kaydetToolStripMenuItem";
+            this.kaydetToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.kaydetToolStripMenuItem.Text = "Kaydet";
+            this.kaydetToolStripMenuItem.Click += new System.EventHandler(this.kaydetToolStripMenuItem_Click);
+            // 
+            // çıkışToolStripMenuItem
+            // 
+            this.çıkışToolStripMenuItem.Name = "çıkışToolStripMenuItem";
+            this.çıkışToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.çıkışToolStripMenuItem.Text = "Çıkış";
+            this.çıkışToolStripMenuItem.Click += new System.EventHandler(this.çıkışToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 93);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(640, 480);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(476, 31);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 56);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(586, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(66, 22);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(360, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Renk Seç";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 40);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Kalem";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(114, 40);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(96, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Daire";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 64);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(96, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Dikdörtgen";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(669, 594);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Paint";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem dosyaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem yeniToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem yükleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kaydetToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem çıkışToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ PaintProgram/PaintProgram/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PaintProgram
+{
+    public partial class Form1 : Form
+    {
+        enum araclar
+        {
+            kalem, 
+            daire,
+            dikdortgen
+        }
+
+        Image img;
+        Bitmap kalem;
+        Color grenk = Color.Blue;
+        Graphics g;
+        int sizeX = 2;
+        int sizeY = 2;
+        bool durum = false;
+        araclar secili = araclar.kalem;
+        Point bas, bit;
+        public Form1()
+        {
+            InitializeComponent();
+            img = new Bitmap(640,480);
+            pictureBox1.Image = img;
+            g = Graphics.FromImage(img);
+            beyazlat(img, 640, 480);
+
+            kalem = new Bitmap(sizeX,sizeY);
+
+            trackBar1.Minimum = 2;
+            trackBar1.Maximum = 32;
+
+        }
+
+        private void beyazlat(Image img, int v1, int v2)
+        {
+            Bitmap k = new Bitmap(v1,v2);
+            for (int i = 0; i < v1; i++)
+                for (int j = 0; j < v2; j++)
+                {
+                    k.SetPixel(i, j, Color.White);
+                }
+            g.DrawImageUnscaled(k, 0, 0);
+            pictureBox1.Image = img;
+
+        }
+
+        private void çıkışToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            switch (secili)
+            {
+                case araclar.kalem: 
+
+                if (e.Button == MouseButtons.Left)
+                {
+                    durum = true;
+                }
+                    break;
+                case araclar.daire:
+                    bas = e.Location;
+                    break;
+                case araclar.dikdortgen:
+                    bas = e.Location;
+
+                    break;
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            switch (secili)
+            {
+                case araclar.kalem:
+
+                    if (e.Button == MouseButtons.Left)
+                        {
+                            durum = false;
+                        }
+                    break;
+                case araclar.daire:
+                    bit = e.Location;
+
+                    DaireCiz(g,bas,bit);
+                    
+                    pictureBox1.Image = img;
+                    break;
+                case araclar.dikdortgen:
+                    bit = e.Location;
+                    DikdortgenCiz(g, bas, bit);
+
+                    pictureBox1.Image = img;
+
+                    break;
+            }
+        }
+
+        private void DikdortgenCiz(Graphics g, Point bas, Point bit)
+        {
+            Pen p = new Pen(grenk);
+            int x, y;
+            x = bas.X;
+            if (x > bit.X) x = bit.X;
+            y = bas.Y;
+            if (y > bit.Y) x = bit.Y;
+
+            g.DrawRectangle(p,x,y,Math.Abs(bit.X-bas.X),Math.Abs(bit.Y- bas.Y));
+        }
+
+        private void DaireCiz(Graphics g, Point bas, Point bit)
+        {
+            Pen p = new Pen(grenk);
+            p.Width = sizeX;
+            g.DrawEllipse(p,bas.X,bas.Y,bit.X - bas.X, bit.Y- bas.Y);
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            switch (secili)
+            {
+                case araclar.kalem:
+
+                    if (e.Button == MouseButtons.Left)
+                    {
+                        CizgiCiz(g, e.Location.X, e.Location.Y,grenk);
+                    }
+                    break;
+                case araclar.daire:
+                    break;
+                case araclar.dikdortgen:
+                    break;
+            }
+        }
+
+        private void CizgiCiz(Graphics g, int x, int y,Color c)
+        {
+            if (durum)
+            {
+                for(int i=0;i<sizeX;i++)
+                    for(int j=0;j<sizeY;j++)
+                    {
+                        kalem.SetPixel(i, j, c);
+                    }
+                g.DrawImageUnscaled(kalem,x,y);
+                pictureBox1.Image = img;
+            }
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            textBox1.Text = trackBar1.Value+"";
+            sizeX = trackBar1.Value;
+            sizeY = trackBar1.Value;
+            kalem = new Bitmap(sizeX, sizeY);
+
+        }
+
+        private void kaydetToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            img.Save("test.jpg",ImageFormat.Jpeg);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var sonuc = colorDialog1.ShowDialog();
+
+            if (sonuc == DialogResult.OK)
+            {
+                grenk = colorDialog1.Color;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            secili = araclar.kalem;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            secili = araclar.daire;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            secili = araclar.dikdortgen;
+        }
+    }
+}
